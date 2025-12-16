bc60f00543a5d8201dc4b93e02cddc0235266690
Sergey Ermolenko <sergey.ermolenko@telexy.com>
2025-12-16 09:28:13 -0800

    FunctionCallData

---
+++ dotnet/extra/ConsoleApp4x/FunctionCallData.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.SemanticKernel;
+
+namespace ConsoleApp4x
+{
+    // TODO: replace KernelArguments with our own classes,
+    // so remote host do not have to refernece semantic kernel
+    /// <summary>
+    /// Helper class for deserializing function call data.
+    /// </summary>
+    public class FunctionCallData
+    {
+        public string Id { get; set; }
+        public string PluginName { get; set; }
+        public string FunctionName { get; set; }
+        public KernelArguments Arguments { get; set; }
+    }
+}
--- dotnet/extra/ConsoleApp4x/RemoteLlmFunctionsHost.cs
+++ dotnet/extra/ConsoleApp4x/RemoteLlmFunctionsHost.cs
@@ -138,18 +138,4 @@ internal class RemoteLlmFunctionsHost
                 throw new InvalidOperationException($"Unexpected JsonElement ValueKind for dicomFileId: {jsonElement.ValueKind}");
         }
     }
-
-
-    // TODO: replace KernelArguments with our own classes,
-    // so remote host do not have to refernece semantic kernel
-    /// <summary>
-    /// Helper class for deserializing function call data.
-    /// </summary>
-    private class FunctionCallData
-    {
-        public string Id { get; set; }
-        public string PluginName { get; set; }
-        public string FunctionName { get; set; }
-        public KernelArguments Arguments { get; set; }
-    }
 }
