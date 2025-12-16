2fb5df7eda0e01bc9908c96db99a2ce23f07ff31
Sergey Ermolenko <sergey.ermolenko@telexy.com>
2025-12-16 08:40:55 -0800

    Plugin Description Generation

---
+++ dotnet/extra/AppTests4x/PluginDescriptionGenerationTest.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Text.Json;
+using ConsoleApp4x;
+
+namespace AppTests4x;
+
+public class PluginDescriptionGenerationTest
+{
+    [Fact]
+    public void CanGeneratePluginDescription()
+    {
+        // Act - Get the generated description
+        var generatedJson = RemoteLlmFunctionsHost.DicomPluginDescriptionJson;
+
+        // Assert - Verify it's valid JSON
+        Assert.NotNull(generatedJson);
+        Assert.NotEmpty(generatedJson);
+
+        // Parse and verify structure
+        var functionDesc = JsonSerializer.Deserialize<RemoteFunctionDescription>(generatedJson);
+
+        Assert.NotNull(functionDesc);
+        Assert.Equal("DicomPlugin", functionDesc.PluginName);
+        Assert.Equal("RegionsJson", functionDesc.FunctionName);
+        Assert.Equal("Returns string representing json containing [(0018,6011) Sequence of Ultrasound Regions] extracted from dicom file.", functionDesc.Description);
+
+        // Verify parameters
+        Assert.NotNull(functionDesc.Parameters);
+        Assert.Single(functionDesc.Parameters);
+
+        var parameter = functionDesc.Parameters[0];
+        Assert.Equal("dicomFileId", parameter.Name);
+        Assert.Equal("Id of the dicom file.", parameter.Description);
+        Assert.Equal("integer", parameter.Type);
+        Assert.True(parameter.IsRequired);
+    }
+
+    [Fact]
+    public void GeneratedJsonCanBeDeserialized()
+    {
+        // Arrange
+        var generatedJson = RemoteLlmFunctionsHost.DicomPluginDescriptionJson;
+
+        // Act - Parse the JSON
+        var jsonDoc = JsonDocument.Parse(generatedJson);
+
+        // Assert - Verify it's a valid JSON document
+        Assert.NotNull(jsonDoc);
+        Assert.NotNull(jsonDoc.RootElement);
+
+        // Verify key properties exist
+        Assert.True(jsonDoc.RootElement.TryGetProperty("PluginName", out _));
+        Assert.True(jsonDoc.RootElement.TryGetProperty("FunctionName", out _));
+        Assert.True(jsonDoc.RootElement.TryGetProperty("Description", out _));
+        Assert.True(jsonDoc.RootElement.TryGetProperty("Parameters", out var parameters));
+
+        // Verify parameters is an array
+        Assert.Equal(JsonValueKind.Array, parameters.ValueKind);
+        Assert.Equal(1, parameters.GetArrayLength());
+    }
+}
--- dotnet/extra/ConsoleApp4x/RemoteLlmFunctionsHost.cs
+++ dotnet/extra/ConsoleApp4x/RemoteLlmFunctionsHost.cs
@@ -1,6 +1,9 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
@@ -15,21 +18,110 @@ namespace ConsoleApp4x;
 /// </summary>
 internal class RemoteLlmFunctionsHost
 {
-    // TODO: create this description from the Plugin's Type
-    public static readonly string DicomPluginDescriptionJson = @"
+    public static readonly string DicomPluginDescriptionJson = CreatePluginDescriptionJson(typeof(DicomPlugin));
+
+    /// <summary>
+    /// Creates a JSON description for a plugin type by extracting metadata from methods decorated with KernelFunctionAttribute.
+    /// </summary>
+    /// <param name="pluginType">The type of the plugin to extract metadata from.</param>
+    /// <returns>A JSON string describing the plugin's functions.</returns>
+    private static string CreatePluginDescriptionJson(Type pluginType)
     {
-        ""PluginName"": ""DicomPlugin"",
-        ""FunctionName"": ""RegionsJson"",
-        ""Description"": ""Returns string representing json containing [(0018,6011) Sequence of Ultrasound Regions] extracted from dicom file."",
-        ""Parameters"": [
+        var pluginName = pluginType.Name;
+        var functions = new List<RemoteFunctionDescription>();
+
+        var methods = pluginType.GetMethods(BindingFlags.Public | BindingFlags.Instance);
+
+        foreach (var method in methods)
+        {
+            var kernelFunctionAttr = method.GetCustomAttribute<KernelFunctionAttribute>();
+            if (kernelFunctionAttr == null)
             {
-                ""Name"": ""dicomFileId"",
-                ""Description"": ""Id of the dicom file."",
-                ""Type"": ""integer"",
-                ""IsRequired"": true
+                continue;
             }
-        ]
-    }";
+
+            var functionName = method.Name;
+            if (functionName.EndsWith("Async", StringComparison.Ordinal) && functionName.Length > "Async".Length)
+            {
+                functionName = functionName.Substring(0, functionName.Length - "Async".Length);
+            }
+
+            var descriptionAttr = method.GetCustomAttribute<DescriptionAttribute>();
+            var description = descriptionAttr?.Description ?? string.Empty;
+
+            var parameters = new List<RemoteParameterDescription>();
+            foreach (var param in method.GetParameters())
+            {
+                var paramDescAttr = param.GetCustomAttribute<DescriptionAttribute>();
+                var paramDesc = paramDescAttr?.Description ?? string.Empty;
+
+                var paramType = GetJsonTypeName(param.ParameterType);
+
+                parameters.Add(new RemoteParameterDescription
+                {
+                    Name = param.Name ?? string.Empty,
+                    Description = paramDesc,
+                    Type = paramType,
+                    IsRequired = !param.IsOptional && !param.HasDefaultValue
+                });
+            }
+
+            functions.Add(new RemoteFunctionDescription
+            {
+                PluginName = pluginName,
+                FunctionName = functionName,
+                Description = description,
+                Parameters = parameters
+            });
+        }
+
+        if (functions.Count == 0)
+        {
+            throw new InvalidOperationException($"No functions with KernelFunctionAttribute found in type {pluginType.Name}");
+        }
+
+        if (functions.Count == 1)
+        {
+            return JsonSerializer.Serialize(functions[0]);
+        }
+
+        return JsonSerializer.Serialize(functions);
+    }
+
+    /// <summary>
+    /// Maps a .NET type to a JSON schema type name.
+    /// </summary>
+    private static string GetJsonTypeName(Type type)
+    {
+        var underlyingType = Nullable.GetUnderlyingType(type) ?? type;
+
+        if (underlyingType == typeof(string))
+        {
+            return "string";
+        }
+        if (underlyingType == typeof(int) || underlyingType == typeof(long) ||
+            underlyingType == typeof(short) || underlyingType == typeof(byte) ||
+            underlyingType == typeof(uint) || underlyingType == typeof(ulong) ||
+            underlyingType == typeof(ushort) || underlyingType == typeof(sbyte))
+        {
+            return "integer";
+        }
+        if (underlyingType == typeof(float) || underlyingType == typeof(double) ||
+            underlyingType == typeof(decimal))
+        {
+            return "number";
+        }
+        if (underlyingType == typeof(bool))
+        {
+            return "boolean";
+        }
+        if (underlyingType.IsArray || typeof(System.Collections.IEnumerable).IsAssignableFrom(underlyingType))
+        {
+            return "array";
+        }
+
+        return "object";
+    }
 
     /// <summary>
     /// Executes a function remotely based on serialized function call information.
